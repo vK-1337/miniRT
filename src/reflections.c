26093d723a480f0ac3a25a4dbe857848a5c924da
Bainur13 <urbain.dumas@gmail.com>
2024-05-30 11:33:42 +0200

    merge

--- src/reflections.c
+++ src/reflections.c
@@ -6,7 +6,7 @@
 /*   By: udumas <udumas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 17:42:34 by vda-conc          #+#    #+#             */
-/*   Updated: 2024/05/30 11:31:45 by udumas           ###   ########.fr       */
+/*   Updated: 2024/05/30 11:33:07 by udumas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,3 @@ t_tuple ft_normal_at(t_sphere sphere, t_tuple world_point)
     world_normal.w = 0;
     return (ft_normalization(world_normal));
 }
-// int main()
-// {
-//     t_sphere s;
-//     t_tuple n;
-//     float **mat;
-    
-//     s = ft_sphere();
-//     mat = translation(0,1,0);
-//     set_transform(&s, mat);
-//     n = ft_normal_at(s, ft_init_tuple(0, 1.70711, -0.70711, 1));
-//     printf("vector n =\nx = %f\ny = %f\nz = %f\nw = %f\n", n.x, n.y,n.z,n.w);
-// }
